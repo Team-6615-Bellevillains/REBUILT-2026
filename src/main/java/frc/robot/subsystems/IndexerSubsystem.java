e7ec93f026f3a1fde7c819cd3799441191615a01
SolveForX475 <ironmuscle475@gmail.com>
2026-02-16 11:49:40 -0500

    start work on new indexer subsystem

---
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IndexerSubsystem extends SubsystemBase {
+    
+    private SparkMax spindexerMotor = new SparkMax(0, MotorType.kBrushless);
+    private SparkMax roadMotor = new SparkMax(0, MotorType.kBrushless);
+    private State state = State.OFF;
+
+    @Override
+    public void periodic() {
+        if (state == State.ON){
+            on();
+        } else {
+            off();
+        }
+    }
+
+    private void on(){
+        spindexerMotor.set(0.5);
+        roadMotor.set(0.5);
+    }
+
+    private void off(){
+        spindexerMotor.stopMotor();
+        roadMotor.stopMotor();
+    }
+
+    public enum State {
+        ON,
+        OFF
+    }
+}
