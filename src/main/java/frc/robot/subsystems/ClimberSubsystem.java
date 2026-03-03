bc4b49eb57a636c09fab6d5af915bffc54b66c90
SolveForX475 <ironmuscle475@gmail.com>
2026-03-02 19:39:32 -0500

    implement climber softlimit, still needs real values

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,7 +1,11 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SoftLimitConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -9,7 +13,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 // TODO: make auton climb
 public class ClimberSubsystem extends SubsystemBase {
-    private SparkMax climbMotor = new SparkMax(40, MotorType.kBrushless); // TODO: put real motor id
+    private SparkMax climbMotor = new SparkMax(40, MotorType.kBrushless);
+
+    public ClimberSubsystem(){
+        SparkMaxConfig config = new SparkMaxConfig();
+        SoftLimitConfig softLimitConfig = new SoftLimitConfig();
+        //TODO: get actual soft-limit values
+        softLimitConfig.forwardSoftLimit(0);
+        softLimitConfig.reverseSoftLimit(0);
+        softLimitConfig.forwardSoftLimitEnabled(true);
+        softLimitConfig.reverseSoftLimitEnabled(true);
+        config.softLimit.apply(softLimitConfig);
+        climbMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
 
     public Command climb(double speed) {
         return Commands.startEnd(() -> climbMotor.set(speed), ()->{climbMotor.stopMotor();});
