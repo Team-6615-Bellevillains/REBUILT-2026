665c9ec14d655b47ec17a031e6922b72f41c9cea
SolveForX475 <ironmuscle475@gmail.com>
2026-03-06 23:13:54 -0500

    fixes

--- src/main/java/frc/robot/utils/MeasureInterpolatingTreeMap.java
+++ src/main/java/frc/robot/utils/MeasureInterpolatingTreeMap.java
@@ -4,27 +4,27 @@ import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Unit;
 
-public class MeasureInterpolatingTreeMap<K extends Unit, U extends Unit> {
+public class MeasureInterpolatingTreeMap<KeyUnit extends Unit, ValueUnit extends Unit> {
 
-    private final InterpolatingTreeMap<Measure<K>, Measure<U>> innerMap;
+    private final InterpolatingTreeMap<Measure<KeyUnit>, Measure<ValueUnit>> innerMap;
 
     public MeasureInterpolatingTreeMap(){
         innerMap = new InterpolatingTreeMap<>(this::measureInverseInterpolator, this::measureInterpolator);
     }
     
-    public double measureInverseInterpolator(Measure<K> startValue, Measure<K> endValue, Measure<K> query){
-        return (query.magnitude() - startValue.magnitude())/(endValue.magnitude() - startValue.magnitude());
-    }
-
-    public Measure<U> measureInterpolator(Measure<U> startValue, Measure<U> endValue, double t){
-        return endValue.minus(startValue).times(t).plus(startValue);
-    }
-
-    public void addPoint(Measure<K> key, Measure<U> value){
+    public void addPoint(Measure<KeyUnit> key, Measure<ValueUnit> value){
         innerMap.put(key, value);
     }
 
-    public Measure<U> getValue(Measure<K> key){
+    public Measure<ValueUnit> getValue(Measure<KeyUnit> key){
         return innerMap.get(key);
+    }
+    
+    private Measure<ValueUnit> measureInterpolator(Measure<ValueUnit> startValue, Measure<ValueUnit> endValue, double t){
+        return endValue.minus(startValue).times(t).plus(startValue);
+    }
+
+    private double measureInverseInterpolator(Measure<KeyUnit> startValue, Measure<KeyUnit> endValue, Measure<KeyUnit> query){
+        return (query.baseUnitMagnitude() - startValue.baseUnitMagnitude())/(endValue.baseUnitMagnitude() - startValue.baseUnitMagnitude());
     }
 }
