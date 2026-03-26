bbde1f4e75bc6495ae43909b25904a2691194347
CelesteMyst <persomrandom7@gmail.com>
2026-03-25 21:52:30 -0400

    more stuff to go here (WONT BE IMPLEMENTED, WORKING ON THIS BECAUSE YES/EXPERIENCE)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -79,7 +79,8 @@ public class RobotContainer {
     // Swerve
     swerveSubsystem.setDefaultCommand(swerveSubsystem.driveCommand(driveAngularVelocity, driverController.povUp()));
     // always aim at hub
-    turretSubsystem.setDefaultCommand(Commands.run(() -> turretSubsystem.aimAtHub(), turretSubsystem)).onlyWhile(swerveSubsystem.inAllianceZone(swerveSubsystem.getPose().getTranslation()));
+    turretSubsystem.setDefaultCommand(Commands.run(() -> turretSubsystem.aimAtHub(), turretSubsystem))
+    .onlyWhile(swerveSubsystem.inAllianceZone(swerveSubsystem.getPose().getTranslation()));
 
     // Driver Controls
     driverController.a().onTrue(swerveSubsystem.resetGyroCommand());
