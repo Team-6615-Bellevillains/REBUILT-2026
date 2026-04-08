0cae4e9ec579c13fc952ad95dafc48142bc9a11c
Ethan Sulea <ethan14145@gmail.com>
2026-04-07 21:03:42 -0400

    Burst shooting only on alliance side.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,4 +1,3 @@
-
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -48,7 +47,8 @@ public class RobotContainer {
             .getDistance(Utils.getHubCenter(
                 DriverStation.getAlliance().orElse(Alliance.Blue)
             ))
-    )
+    ),
+    () -> Utils.isInAllianceZone(swerveSubsystem.getPose())
   );
   IntakeSubsystem  intakeSubsystem  = new IntakeSubsystem(swerveSubsystem::getRobotRelativeVelocity);
   TurretSubsystem  turretSubsystem  = new TurretSubsystem(swerveSubsystem::getPose);
